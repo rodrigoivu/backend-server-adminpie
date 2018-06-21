c7415d39187b4a082e6f0f35a1a09c4fec2d715b
Rodrigo Varas Urbina <rvaras@iMac-de-Rodrigo.local>
2018-06-20 22:58:21 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var express = require ('express');
+var bodyParser = require ('body-parser');
+
+var app = express();
+
+// cargar rutas
+
+//var user_routes = require('./routes/user');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// configurar cabeceras http
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin','*'); //permite el acceso a todos los dominios, a las apis
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-Whith, Content-Type, Accept,Access-Control-Allow-Request-Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+
+	next();
+});
+
+//rutas base
+//app.use('/api', user_routes);
+
+
+
+module.exports = app;
